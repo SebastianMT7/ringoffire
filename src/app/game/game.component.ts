aa81cc6421a603b6f4e76f8ff79db75d424ade0b
Sebastian Torney <STorney@web.de>
2024-06-05 22:13:54 +0200

    sth

---
+++ src/app/game/game.component.ts
@@ -0,0 +1,39 @@
+import { CommonModule } from '@angular/common';
+import { Component, OnInit } from '@angular/core';
+import { Game } from '../../models/game';
+
+@Component({
+  selector: 'app-game',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './game.component.html',
+  styleUrl: './game.component.scss'
+})
+export class GameComponent implements OnInit {
+  pickCardAnimation = false;
+  currentCard: string ='';
+  game: Game = new Game();
+
+  ngOnInit(): void {
+    this.newGame();
+    console.log(this.game)
+  }
+
+  constructor() { }
+
+  newGame() {
+    this.game = new Game();
+  }
+
+  takeCard() {
+    if (!this.pickCardAnimation) {
+      this.currentCard = this.game.stack.pop()as string;
+      this.pickCardAnimation = true;
+
+      setTimeout(() => {
+        this.game.playedCards.push(this.currentCard);
+        this.pickCardAnimation = false;
+      }, 1000);
+    }
+  }
+}
